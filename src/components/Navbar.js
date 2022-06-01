e6c4b6b270006bcdc36ed37ca147ca3c80d349ca
Surabhi S <63139488+SurabhiSuresh22@users.noreply.github.com>
2022-06-01 23:01:42 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div>        
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">Yum Foods</a>
+        <p className="navbar-brand">Yum Foods</p>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
