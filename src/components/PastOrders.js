f8f15428498f2a7b43146d7dc0af911ccd7c8b35
SurabhiSuresh22 <surahi2207200@gmail.com>
2022-06-01 22:48:26 +0530

    final1commit

--- src/components/PastOrders.js
+++ src/components/PastOrders.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './PastOrders.css'
 
-const PastOrders = () => {
+const PastOrders = ({past}) => {
+  // console.log(past)
   return (
-    <div>
+    <div className='PO'>
       <h2>PastOrders</h2>
       <Link className="nav-item nav-link" to='/'>Home</Link>
+      <div className="pastOrders">
+          {past.map((e)=>{
+            return <h5>{e}</h5>
+          })}
+      </div>
     </div>
 
   )
